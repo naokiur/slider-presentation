44bd75c0b8b9ecd4b7dd9e4bd46baef44fa35833
naokiur <atomic.transfer.comp@gmail.com>
2017-05-04 16:22:34 +0900

    Add state to Slide component.

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
 
 export default class Slide extends Component {
+    constructor() {
+        super();
+        this.state = {
+            hidden: true
+        };
+    }
+
     render() {
+        const slideStyle = this.state.hidden ? {display: 'none'} : {};
 
         return (
-            <div>Hello {this.props.name}, {this.props.color}!!</div>
+            <div style={slideStyle}>Hello {this.props.name}, {this.props.color}!!</div>
         )
     }
 }
