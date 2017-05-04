5b208d63ba96f56329e82adf04b7483d041aadad
naokiur <atomic.transfer.comp@gmail.com>
2017-05-04 15:08:30 +0900

    Add dynamic components from JSON data.

--- src/components/SlideRoot.js
+++ src/components/SlideRoot.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import Slide from './Slide.js'
 
 export default class SlideRoot extends Component {
+
     render() {
+        const data = [
+            {name: 'aaa', color: 'white'},
+            {name: 'bbb', color: 'black'}
+
+        ];
+
         const divStyle = {
             color : 'red',
             margin : '5%'
         };
 
         return (
-            <div style={divStyle}>Hello SlideRoot!!</div>
+            <div style={divStyle}>
+                {
+                    data.map((d) => (
+                        <Slide name={d.name} color={d.color} />
+                    ))
+                }
+
+                Hello SlideRoot!!</div>
         )
     }
 }
