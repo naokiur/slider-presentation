644e06a7561c743c992df0b486587e56f50d6666
naokiur <atomic.transfer.comp@gmail.com>
2017-05-04 14:21:32 +0900

    Separate first component.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SlideRoot from './components/SlideRoot.js';
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,21 +11,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <SlideRoot />
+        <SlideRoot/>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
   }
-}
-
-class SlideRoot extends Component {
-    render() {
-        return (
-            <div>Hello SlideRoot!!</div>
-        )
-    }
 }
 
 export default App;
