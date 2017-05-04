e37efb1a214ec438303a10914352d6fb4de9d90e
naokiur <atomic.transfer.comp@gmail.com>
2017-05-04 20:53:30 +0900

    Add manuscript for this time.

--- src/components/SlideRoot.js
+++ src/components/SlideRoot.js
@@ -11,7 +11,11 @@ export default class SlideRoot extends Component {
 
     render() {
         const data = [
-
+                {title: '今日やったこと', contents: ['Naoki Shimura (Github: naokiur)']},
+                {title: '自己紹介', contents: ['SIerです。お客様先に常駐して、QA回答おじさんやっています。', 'フロントエンドは、jQueryで止まってしまっているので、更新したくて参加致しました。']},
+                {title: 'できたこと', contents: ['Reactプロジェクトの作り方', 'コンポーネントの作り方', 'stateの変更の仕方', 'Reactライフサイクルの一つ(stateを変更すると、当該コンポーネントが再描画される)']},
+                {title: 'できなかったこと', contents: ['親コンポーネントのstate変更時の、子コンポーネントstate変更の仕方','(そもそも、コンポーネントの分け方がよくない？)']},
+                {title: '成果物', contents: ['このスライド描画アプリです。']}
         ];
 
         const divStyle = {
